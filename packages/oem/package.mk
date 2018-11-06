52180c82eeb4ec9d919b44b3dc0d50641e9c82a2
alexelec <alexelecv5@gmail.com>
2018-11-06 10:04:32 +0200

    torrserver: add pkg

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -13,3 +13,4 @@ PKG_LONGDESC="OEM: Metapackage for various OEM packages"
 # torrent services
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET scan-m3u"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET acestream-rpi"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET torrserver"
