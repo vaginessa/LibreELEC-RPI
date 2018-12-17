c4a0edd816fe000fbcb0a75a59df81d1348580bf
alexelec <alexelecv5@gmail.com>
2018-12-17 16:38:19 +0200

    tvheadend: bump to v4.3-1677

--- packages/xmedia/tvservice/tvheadend/package.mk
+++ packages/xmedia/tvservice/tvheadend/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2011-present Alex@ELEC (http://alexelec.in.ua)
 
 PKG_NAME="tvheadend"
-PKG_VERSION="91f6de4"
+PKG_VERSION="99461b8"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
 PKG_URL=""
